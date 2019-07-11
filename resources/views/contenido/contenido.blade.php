236caca85855b2f475917835546b8107186106d1
Ningune Persona <elniko05@outlook.com>
2019-07-11 12:00:42 -0500

    Elysiam + tareas de gantt 9 y 10 JUL

---
+++ resources/views/contenido/contenido.blade.php
@@ -0,0 +1,15 @@
+@extends('principal')
+    @section('contenido')
+
+
+    <template v-if="menu==1">
+    <h1>-------------------------------------------------------------------------------</h1>
+    </template>
+
+
+
+
+
+
+
+    @endsection
