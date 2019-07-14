60b3598e31c7a32ca617fdca0ed599574912b628
DECQ04 <35268239+DECQ04@users.noreply.github.com>
2019-07-13 19:48:58 -0500

    vistas, modelos, controladores de Proyectos

---
+++ app/Http/Controllers/proyectoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Proyecto;
+
+class proyectoController extends Controller
+{
+    public function inicio(){
+        
+        $proyectos = Proyecto::all();
+    
+        return view('contenido/prueba',['proyectos'=>$proyectos]);
+    } 
+  
+
+
+    public function store(Request $request)
+    {
+        $proyectos = new Proyecto(); 
+        $proyectos->id_manager=request('id_manager');
+        $proyectos->id_cliente=request('id_cliente');
+        $proyectos->titulo=request('titulo');
+        $proyectos->fecha_incio=request('fecha_inicio');
+        $proyectos->fecha_vencimiento=request('fecha_vencimiento');
+        $proyectos->pago_total=request('pago_total');
+        $proyectos->id_pago=request('id_pago');
+        $proyectos->estado= '1';
+        $proyectos->save();
+        return redirect('/prueba');
+        //return request()->all();//trae todo del form categoria/create
+        //return request('nombre'); este el nombre
+        //return request('descripcion'); y asi 
+    
+
+    }
+   
+}
