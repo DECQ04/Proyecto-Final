e09880b0c98c813113fb0b897cd66913f86088f8
DECQ04 <35268239+DECQ04@users.noreply.github.com>
2019-07-13 19:41:16 -0500

    vistas, modelos, controladores de Proyectos

--- routes/web.php
+++ routes/web.php
@@ -10,38 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('principal');
-   
-=======
-    return view('index');
->>>>>>> d26a775d27b5c8032789acc9f498d72516d8344d
 });
-Route::get('/tablas', function () {
-    return view('tablas');
-   
-});
-<<<<<<< HEAD
-Route::get('/tareas', function () {
-    return view('tareas');
-   
-});
-Route::get('/factura', function () {
-    return view('factura');
-});
-Route::get('/modales', function () {
-    return view('modales');
-});/*
-=======
->>>>>>> d26a775d27b5c8032789acc9f498d72516d8344d
 
-Route::get('/tarea','tareaController@index');
-        Route::post('/tarea/registrar', 'tareaController@store');
-        Route::put('/tarea/actualizar', 'tareaController@update');
-        Route::put('/tarea/desactivar', 'tareaController@desactivar');
-        Route::put('/tarea/activar', 'tareaController@activar');
-        Route::get('/tarea/buscarTarea', 'tareaController@buscarTarea');
-        Route::get('/tarea/listarTarea', 'tareaController@listarTarea');
+Route::get('/prueba','proyectoController@inicio');
+Route::post('/prueba','proyectoController@store');
          
