4a3348f40c8897f39ccc97e0eeac76008f00981a
DECQ04 <35268239+DECQ04@users.noreply.github.com>
2019-07-17 15:23:12 -0500

    Actualizacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,47 @@
 Route::get('/', function () {
     return view('principal');
 });
-Route::resource('clientes','ControllerCliente');
+
+Route::get('/', function () {
+    return view('principal');
+});
+
+Route::get('/proyectos','ControllerProyecto@inicio');
+Route::post('/proyectos','ControllerProyecto@store');
+Route::get('/{proyed}/proyectos','ControllerProyecto@desactivar');
+Route::get('/{proyed}/act','ControllerProyecto@activar');
+
+Route::get('/tareas','ControllerTarea@inicio');
+Route::post('/tareas','ControllerTarea@store');
+Route::get('/{proyed}/tareas','ControllerTarea@desactivar');
+Route::get('/{proyed}/tareasact','ControllerTarea@activar');
+
+Route::get('/gastos','ControllerGasto@inicio');
+Route::post('/gastos','ControllerGasto@store');
+Route::get('/{proyed}/gastos','ControllerGasto@desactivar');
+Route::get('/{proyed}/gastosact','ControllerGasto@activar');
+
+Route::get('/pagos','ControllerPago@inicio');
+Route::post('/pagos','ControllerPago@store');
+Route::get('/{proyed}/pagos','ControllerPago@desactivar');
+Route::get('/{proyed}/pagosact','ControllerPago@activar');
+         
+Route::get('/colaboradores','ControllerColaborador@inicio');
+Route::post('/colaboradores','ControllerColaborador@store');
+Route::get('/{proyed}/colaboradores','ControllerColaborador@desactivar');
+Route::get('/{proyed}/colaboradoresact','ControllerColaborador@activar');
+
+Route::get('/clientes','ControllerCliente@inicio');
+Route::post('/clientes','ControllerCliente@store');
+Route::get('/{proyed}/clientes','ControllerCliente@desactivar');
+Route::get('/{proyed}/clientesact','ControllerCliente@activar');
+
+Route::get('/manager','ControllerManager@inicio');
+Route::post('/manager','ControllerManager@store');
+Route::get('/{proyed}/manager','ControllerManager@desactivar');
+Route::get('/{proyed}/manageract','ControllerManager@activar');
+
+/*Route::resource('clientes','ControllerCliente');
 
 //Trae el (index)
 //Route::get('/clientes','ControllerCliente@inicio');
@@ -48,7 +88,7 @@ Route::resource('pagos','ControllerPago');
          
 Route::resource('colaboradores','ControllerColaborador');
 //Route::get('/colaboradores','ControllerColaborador@inicio');
-//Route::post('/colaboradores','ControllerColaborador@store');
+//Route::post('/colaboradores','ControllerColaborador@store');*/
 
 
          
