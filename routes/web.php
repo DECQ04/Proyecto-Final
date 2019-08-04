37ba627425d38c50da938fb630086d4e509ad9a1
DECQ04 <35268239+DECQ04@users.noreply.github.com>
2019-08-03 23:18:26 -0500

    Actualizacion de rutas controladas

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,20 @@ Route::get('/', function () {
  
 Route::get('/perfil', function () {
     return view('contenido.personal');
-});
+})->middleware('auth');
 Route::get('/reportes', function () {
     return view('contenido.reportes');
-});
+})->middleware('auth');
 Route::get('/perfilup','ControllerPerfil@up');
 
-Route::get('/tickets','ControllerTickets@inicio');
+Route::get('/tickets','ControllerTickets@inicio')->middleware('auth');;
 Route::post('/tickets','ControllerTickets@store');
 Route::post('/verTicket','ControllerTickets@verticket');
 Route::post('/ticketsRespuesta','ControllerTickets@responderTickets');
 
-Route::get('/proyectos','ControllerProyecto@inicio');
-Route::get('/proyectosDC','ControllerProyecto@inicioDC');
-Route::post('/proyectos','ControllerProyecto@store');
+Route::get('/proyectos','ControllerProyecto@inicio')->middleware('auth');
+Route::get('/proyectosDC','ControllerProyecto@inicioDC')->middleware('auth');;
+Route::post('/proyectos','ControllerProyecto@store')->middleware('auth');
 Route::post('/proyectosEdit','ControllerProyecto@edit');
 Route::post('/proyectosup','ControllerProyecto@update');
 Route::get('/{proyed}/proyectos','ControllerProyecto@desactivar');
@@ -38,8 +38,8 @@ Route::get('/{proyed}/act','ControllerProyecto@activar');
 Route::get('/reportes','ControllerProyecto@reportes');
 Route::post('/reportesver','ControllerProyecto@VisualizarReporte');
 
-Route::get('/tareas','ControllerTarea@inicio');
-Route::get('/tareasDC','ControllerTarea@inicioDC');
+Route::get('/tareas','ControllerTarea@inicio')->middleware('auth');;
+Route::get('/tareasDC','ControllerTarea@inicioDC')->middleware('auth');;
 Route::post('/tareas','ControllerTarea@store');
 Route::post('/tareasEdit','ControllerTarea@edit');
 Route::post('/tareasup','ControllerTarea@update');
@@ -48,31 +48,31 @@ Route::get('/{proyed}/tareasact','ControllerTarea@activar');
 Route::get('/{proyed}/tareasfin','ControllerTarea@fin');
 Route::get('/{proyed}/tareasAct','ControllerTarea@act');
 
-Route::get('/gastos','ControllerGasto@inicio');
+Route::get('/gastos','ControllerGasto@inicio')->middleware('auth');;
 Route::post('/gastos','ControllerGasto@store');
 Route::post('/gastosEdit','ControllerGasto@edit');
 Route::post('/gastosup','ControllerGasto@update');
 Route::get('/{proyed}/gastos','ControllerGasto@desactivar');
 Route::get('/{proyed}/gastosact','ControllerGasto@activar');
 
-Route::get('/pagos','ControllerPago@inicio');
+Route::get('/pagos','ControllerPago@inicio')->middleware('auth');;
 Route::post('/pagos','ControllerPago@store');
 Route::post('/pagosEdit','ControllerPago@edit');
 Route::post('/pagosup','ControllerPago@update');
 Route::get('/{proyed}/pagos','ControllerPago@desactivar');
 Route::get('/{proyed}/pagosact','ControllerPago@activar');
          
-Route::get('/colaboradores','ControllerColaborador@inicio');
+Route::get('/colaboradores','ControllerColaborador@inicio')->middleware('auth');;
 Route::post('/colaboradores','ControllerColaborador@store');
 Route::get('/{proyed}/colaboradores','ControllerColaborador@desactivar');
 Route::get('/{proyed}/colaboradoresact','ControllerColaborador@activar');
 
-Route::get('/clientes','ControllerCliente@inicio');
+Route::get('/clientes','ControllerCliente@inicio')->middleware('auth');;
 Route::post('/clientes','ControllerCliente@store');
 Route::get('/{proyed}/clientes','ControllerCliente@desactivar');
 Route::get('/{proyed}/clientesact','ControllerCliente@activar');
 
-Route::get('/manager','ControllerManager@inicio');
+Route::get('/manager','ControllerManager@inicio')->middleware('auth');;
 Route::post('/manager','ControllerManager@store');
 Route::post('/manager/actualizar','ControllerManager@update');
 Route::get('/{proyed}/manager','ControllerManager@desactivar');
@@ -82,5 +82,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home','ControllerProyecto@cont');
-Route::get('/principal','ControllerProyecto@cont');
+Route::get('/principal','ControllerProyecto@cont')->middleware('auth');;
 Route::get('/regresar', 'HomeController@regresar');
